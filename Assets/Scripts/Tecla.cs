38ee3786422361f8d7d6fd70c0cdb296d96ec3e3
WaterWhirl <leonardo-zende@hotmail.com>
2022-05-18 19:52:23 -0300

    Script Som

---
+++ Assets/Scripts/Tecla.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tecla : MonoBehaviour
+{
+
+    public string tecla;
+
+    void Update()
+    {
+        if (!tecla.Equals(""))
+        {
+            if (Input.GetKeyDown(tecla))
+                GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<AudioSource>.()Play();
+    }
+}
